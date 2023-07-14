a5093d09b4246768329d354ed683717d04458c24
MirageCentury <victorien.vanroye@gmail.com>
2023-07-14 10:38:28 +0200

    feat: prepare branch with placeholder code

---
+++ src/harvester.manager.js
@@ -0,0 +1,10 @@
+// This module is responsible to globally manage harvest operation
+
+module.exports = {
+    init(){
+
+    },
+    loop(){
+        
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+// Main - Entrypoint of the game
+
+var harvester_manager = require("harvester.manager")
+
+// Init for manager
+harvester_manager.init()
+
 module.exports.loop = function () {
     console.log('groot - loop - start')
-    
+    // Loop of Manager
+    harvester_manager.loop()
+    //
     console.log('groot - loop - end')   
 }
